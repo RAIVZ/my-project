dc978b0fb674ed27e97e6fdcfb3ca24d55c81fe3
ТВОЁ_ИМЯ <87667107+RAIVZ@users.noreply.github.com>
2025-03-14 20:37:23 +0300

    регулярки

--- js.js
+++ js.js
@@ -4,3 +4,45 @@ const nav = document.querySelector(".nav-list");
 burger.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+// const balance = document.querySelector("#balance");
+
+// const replenish = document.querySelector(".grid-replenish");
+
+// const take = document.querySelector(".grid-take");
+
+// const examination = document.querySelector(".grid-examination");
+
+
+// replenish.addEventListener("click", function () {
+//   let currentBalance = balance.value;  
+//   currentBalance = parseInt(currentBalance.substring(8, currentBalance.length - 1));
+//   let amount = parseInt(prompt("Введите сумму пополнения:"));
+
+//   if (!isNaN(amount) && amount > 0) {
+//     currentBalance += amount;
+//     balance.value = "Баланс: " + currentBalance + "р";
+// } else {
+//     alert("Некорректная сумма!");
+// }
+// });
+
+// take.addEventListener("click", function () {
+//   let currentBalance = balance.value;  
+//   currentBalance = parseInt(currentBalance.substring(8, currentBalance.length - 1));
+//   let amount = parseInt(prompt("Введите сумму для снятия:"));
+
+//   if (!isNaN(amount) && amount > 0) {
+//     currentBalance -= amount;
+//     balance.value = "Баланс: " + currentBalance + "р";
+// } else {
+//     alert("Некорректная сумма!");
+// }
+// });
+
+// examination.addEventListener("click", function () {
+//   let currentBalance = balance.value;  
+//   currentBalance = parseInt(currentBalance.substring(8, currentBalance.length - 1));
+//   alert(`Ваш баланс составляет: ${currentBalance}`);
+// });
+
